b65b96ac4ae26eefee0958ec222313cdcbf33868
leo-moller <leomollerfernandez@gmail.com>
2025-04-19 20:10:46 -0300

    Fazendo o main e a interface

---
+++ interface.py
@@ -0,0 +1,15 @@
+from tkinter import * 
+from tkinter import ttk
+from main import *
+
+
+janela = Tk()
+janela.geometry("800x800")
+janela.title("Timer")
+janela.config(bg="#454545")
+
+
+
+
+janela.mainloop()
+
